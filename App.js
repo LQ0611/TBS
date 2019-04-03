caee4ba4919b488ed6ddcd328c0deb05cb0551d6
liqiang <liqiang@epipechina.com>
2019-04-03 10:13:40 +0800

    second update

--- App.js
+++ App.js
@@ -30,30 +30,12 @@ export default class App extends Component<Props> {
     }
 
   render() {
-      let fileHTML = "http://192.168.100.21:8102/jxtrq//OA_LNG_SND_GWNG/2018/2018-12-12/MTgxMjEyODI5NTIwNjQy/MTgxMjEyODI5NTIwNjQy.html";
-      let file = "http://192.168.100.21:8102/file/3QIsoqd292boebgSMNqvSle.docx";
-      let file2 = "http://192.168.100.21:8102/file/2laJBpBkR7Z8FyDZIglWzd.xlsx";
-      let file3 = "http://192.168.100.21:8102/file/1n6ZqBERNatqfcDj8NsjCNc.pdf";
-      let file4 = "http://192.168.100.21:8102/file/2emdjqTvp82FiLX9YIuxMk.docx";
-      let file5 = "http://gs.moli.ds.lunbocl.ott.cibntv.net/hls/vCIBNHSYX/1800/vCIBNHSYX.m3u8?md5=c246ac4e6b6cb88b7f970b530ff04b86";
-    return (
-        <ScrollView>
+      let file3 = "http://grad.cnu.edu.cn/docs/2019-04/20190403003048351438.pdf";
+      return (
+
       <View style={styles.container}>
 
-          <View style={{alignItems:'center',marginBottom:20}}>
-              <Text style={{fontSize:30,color:'#333333',}}
-                    onPress={()=>this.call_button(file)}   //add
-              >
-                  文件 word 测试
-              </Text>
-          </View>
-          <View style={{alignItems:'center',marginBottom:20}}>
-              <Text style={{fontSize:30,color:'#333333',}}
-                    onPress={()=>this.call_button(file2)}   //add
-              >
-                  文件2 excel 测试
-              </Text>
-          </View>
+
           <View style={{alignItems:'center',marginBottom:20}}>
               <Text style={{fontSize:30,color:'#333333',}}
                     onPress={()=>this.call_button(file3)}   //add
@@ -61,30 +43,6 @@ export default class App extends Component<Props> {
                   文件3 pdf 测试
               </Text>
           </View>
-          <View style={{alignItems:'center',marginBottom:20}}>
-              <Text style={{fontSize:30,color:'#333333',}}
-                    onPress={()=>this.call_button(file4)}   //add
-              >
-                  文件4 word 测试
-              </Text>
-          </View>
-
-          <View style={{alignItems:'center',marginBottom:20}}>
-              <Text style={{fontSize:30,color:'#333333',}}
-                    onPress={()=>this.call_button(fileHTML)}   //add
-              >
-                  测试永忠转化后的文件
-              </Text>
-          </View>
-
-          <View style={{alignItems:'center',marginBottom:20}}>
-              <Text style={{fontSize:30,color:'#333333',}}
-                    onPress={()=>this.call_button(file5)}   //add
-              >
-                  测试视频
-              </Text>
-          </View>
-
 
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -96,7 +54,7 @@ export default class App extends Component<Props> {
           {instructions}
         </Text>
       </View>
-        </ScrollView>
+
     );
   }
 }
